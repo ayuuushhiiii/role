ef32a1342f634a9388117495bfc3d57d41233279
Ayushi Saxena <Ayushi.Saxena@insight.com>
2026-01-08 23:47:07 +0530

    testing without dynamo db

--- accounts/netzero/backend.tf
+++ accounts/netzero/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "amazonbucket0011"
     key            = "github-actions/tf.state"
     region         = "us-east-1"
-    dynamodb_table = "tf-state-table"
+    #dynamodb_table = "tf-state-table"
     encrypt        = true
   }
 }
